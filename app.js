bfc195f699b819f937f4b5365b90543816f9ef33
Chelsea Pae <chelseapae@gmail.com>
2022-07-11 15:30:47 -0700

    Complete Exercise: React Components

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 class GroceryListItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
   }
 
   render () {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
     return (
-      <li>{this.props.item}</li>
-    );
+        <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+      );
 }
 }
-
 
 var GroceryList = (props) => {
   return (<ul id = 'app'>
@@ -23,22 +34,3 @@ var groceries = ['Avocado', 'Tortillas', 'Hot Sauce']
 
 
 ReactDOM.render(<GroceryList groceries={groceries}/>, document.getElementById("app"))
-
-// ReactDOM.render(<GroceryListItem />, document.getElementById("app"))
-
-// var GroceryListItem = (props) => {
-//   return (
-//   <ul id = 'app'>
-//   <li>{props.item[0]}</li>
-//   <li>{props.item[1]}</li>
-//   <li>{props.item[2]}</li>
-// </ul>
-// )
-// };
-
-// var GroceryList = () => (
-//   <div>
-//     <h2>My Grocery List</h2>
-//     <GroceryListItem item={['Avocado', 'Tortillas', 'Hot Sauce']}/>
-//   </div>
-// );
