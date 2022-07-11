b3039325c47d8ebc89f8edef291184af3676470c
Chelsea Pae <chelseapae@gmail.com>
2022-07-10 23:08:07 -0700

    Create React Components insides of GroceryList Component

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// TODO
+var GroceryList = () => (
+  <ul id = 'app'>
+    <Apples />
+    <Bananas />
+  </ul>
+);
+
+var Apples = () => (
+  <li>Apples</li>
+);
+
+var Bananas = () => (
+  <li>Bananas</li>
+);
+
+var GroceryListItem = (props) => (
+  <ul id = 'app'>
+    <li>{props.item[0]}</li>
+    <li>{props.item[1]}</li>
+    <li>{props.item[2]}</li>
+  </ul>
+);
+
+
+var Items = () => (
+  <div>
+    <GroceryListItem item={['Avocado', 'Tortillas', 'Hot Sauce']}/>
+  </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"))
+//ReactDOM.render(<GroceryListItem />, document.getElementById("app"))
+// ReactDOM.render(<Items />, document.getElementById("app"))
